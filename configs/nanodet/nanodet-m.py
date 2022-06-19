b33d588d6f249d288d579a92694a82a1a980aa37
lihao4 <lihao4@sensetime.com>
2022-06-19 23:12:27 +0800

    update test pipeline

--- configs/nanodet/nanodet-m.py
+++ configs/nanodet/nanodet-m.py
@@ -6,3 +6,25 @@ _base_ = [
 ]
 # optimizer
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='Resize',
+        img_scale=(320, 320),
+        keep_ratio=True),
+    dict(
+        type='Normalize',
+        mean=[103.53, 116.28, 123.675],
+        std=[57.375, 57.12, 58.395],
+        to_rgb=True),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='Collect', keys=['img'])
+]
+
+data_root = 'D:/dataset/coco'
+data = dict(
+    test=dict(
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline))
